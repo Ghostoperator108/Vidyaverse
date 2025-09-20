07b3a25ce29b4974a3fdb7948a22e5ee40662470
Ghostoperator108 <aninditabhattacharjee40609@gmail.com>
2025-09-20 14:40:43 +0530

    Some necessary changes ha been made

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,6 @@ import React from 'react';
 import { HashRouter, Routes, Route } from 'react-router-dom';
 import WelcomePage from './Components/WelcomePage';
 import LoginPage from './Components/LoginPage';
-import DashboardPage from './Components/StudentDashboard/DashboardPage';
 
 
 function App() {
@@ -11,7 +10,8 @@ function App() {
             <Routes>
                 <Route path="/" element={<WelcomePage />} />
                 <Route path="/login" element={<LoginPage />} />
-                <Route path="/dashboard" element={<DashboardPage />} />
+                
+                
             </Routes>
         </HashRouter>
     );
